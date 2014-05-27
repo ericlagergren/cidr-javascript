d0519c6dbf4a4091c47f96bd6fdedf87f8158ce4
Eric <ericscottlagergren@gmail.com>
2014-05-27 02:38:43 -0700

    k

---
+++ full.js
@@ -0,0 +1,252 @@
+function val() {
+    (function() {
+        var elements = document.getElementsByClassName('ugh');
+for (var i in elements) {
+  if (elements.hasOwnProperty(i)) {
+    elements[i].className = 'meow';
+  }
+}
+    }());
+    (function() {
+        var e = document.cidrform.cidr.value,
+            j = document.getElementById("ij"),
+            t = document.getElementById("sm");
+        if (e === "/0" || e == 0 ) {j.innerHTML = "0.0.0.0"; t.innerHTML = 0;};
+        if (e === "/1" || e == 1 ) {j.innerHTML = "128.0.0.0"; t.innerHTML = 1;};
+        if (e === "/2" || e == 2 ) {j.innerHTML = "192.0.0.0"; t.innerHTML = 2;};
+        if (e === "/3" || e == 3 ) {j.innerHTML = "224.0.0.0"; t.innerHTML = 3;};
+        if (e === "/4" || e == 4 ) {j.innerHTML = "240.0.0.0"; t.innerHTML = 4;};
+        if (e === "/5" || e == 5 ) {j.innerHTML = "248.0.0.0"; t.innerHTML = 5;};
+        if (e === "/6" || e == 6 ) {j.innerHTML = "252.0.0.0"; t.innerHTML = 8;};
+        if (e === "/7" || e == 7 ) {j.innerHTML = "254.0.0.0"; t.innerHTML = 7;};
+        if (e === "/8" || e == 8 ) {j.innerHTML = "255.0.0.0"; t.innerHTML = 8;};
+        if (e === "/9" || e == 9 ) {j.innerHTML = "255.128.0.0"; t.innerHTML = 9;};
+        if (e === "/10" || e == 10 ) {j.innerHTML = "255.192.0.0"; t.innerHTML = 10;};
+        if (e === "/11" || e == 11 ) {j.innerHTML = "255.224.0.0"; t.innerHTML = 11;};
+        if (e === "/12" || e == 12 ) {j.innerHTML = "255.240.0.0"; t.innerHTML = 12;};
+        if (e === "/13" || e == 13 ) {j.innerHTML = "255.248.0.0"; t.innerHTML = 13;};
+        if (e === "/14" || e == 14 ) {j.innerHTML = "255.252.0.0"; t.innerHTML = 14;};
+        if (e === "/15" || e == 15 ) {j.innerHTML = "255.254.0.0"; t.innerHTML = 15;};
+        if (e === "/16" || e == 16 ) {j.innerHTML = "255.255.0.0"; t.innerHTML = 16;};
+        if (e === "/17" || e == 17 ) {j.innerHTML = "255.255.128.0"; t.innerHTML = 17;};
+        if (e === "/18" || e == 18 ) {j.innerHTML = "255.255.192.0"; t.innerHTML = 18;};
+        if (e === "/19" || e == 19 ) {j.innerHTML = "255.255.224.0"; t.innerHTML = 19;};
+        if (e === "/20" || e == 20 ) {j.innerHTML = "255.255.240.0"; t.innerHTML = 20;};
+        if (e === "/21" || e == 21 ) {j.innerHTML = "255.255.248.0"; t.innerHTML = 21;};
+        if (e === "/22" || e == 22 ) {j.innerHTML = "255.255.252.0"; t.innerHTML = 22;};
+        if (e === "/23" || e == 23 ) {j.innerHTML = "255.255.254.0"; t.innerHTML = 23;};
+        if (e === "/24" || e == 24 ) {j.innerHTML = "255.255.255.0"; t.innerHTML = 24;};
+        if (e === "/25" || e == 25 ) {j.innerHTML = "255.255.255.128"; t.innerHTML = 25;};
+        if (e === "/26" || e == 26 ) {j.innerHTML = "255.255.255.192"; t.innerHTML = 26;};
+        if (e === "/27" || e == 27 ) {j.innerHTML = "255.255.255.224"; t.innerHTML = 27;};
+        if (e === "/28" || e == 28 ) {j.innerHTML = "255.255.255.240"; t.innerHTML = 28;};
+        if (e === "/29" || e == 29 ) {j.innerHTML = "255.255.255.248"; t.innerHTML = 29;};
+        if (e === "/30" || e == 30 ) {j.innerHTML = "255.255.255.252"; t.innerHTML = 30;};
+        if (e === "/31" || e == 31 ) {j.innerHTML = "255.255.255.254"; t.innerHTML = 31;};
+        if (e === "/32" || e == 32 ) {j.innerHTML = "255.255.255.255"; t.innerHTML = 32;};
+
+    }());
+    (function() {
+        var e = document.cidrform.submask.value,
+            t = document.getElementById("sm"),
+            j = document.getElementById("ij");
+        if (e == "0.0.0.0") {t.innerHTML = 0; j.innerHTML = e;};
+        if (e == "128.0.0.0") {t.innerHTML = 1; j.innerHTML = e;};
+        if (e == "192.0.0.0") {t.innerHTML = 2; j.innerHTML = e;};
+        if (e == "224.0.0.0") {t.innerHTML = 3; j.innerHTML = e;};
+        if (e == "240.0.0.0") {t.innerHTML = 4; j.innerHTML = e;};
+        if (e == "248.0.0.0") {t.innerHTML = 5; j.innerHTML = e;};
+        if (e == "252.0.0.0") {t.innerHTML = 8; j.innerHTML = e;};
+        if (e == "254.0.0.0") {t.innerHTML = 7; j.innerHTML = e;};
+        if (e == "255.0.0.0") {t.innerHTML = 8; j.innerHTML = e;};
+        if (e == "255.128.0.0") {t.innerHTML = 9; j.innerHTML = e;};
+        if (e == "255.192.0.0") {t.innerHTML = 10; j.innerHTML = e;};
+        if (e == "255.224.0.0") {t.innerHTML = 11; j.innerHTML = e;};
+        if (e == "255.240.0.0") {t.innerHTML = 12; j.innerHTML = e;};
+        if (e == "255.248.0.0") {t.innerHTML = 13; j.innerHTML = e;};
+        if (e == "255.252.0.0") {t.innerHTML = 14; j.innerHTML = e;};
+        if (e == "255.254.0.0") {t.innerHTML = 15; j.innerHTML = e;};
+        if (e == "255.255.0.0") {t.innerHTML = 16; j.innerHTML = e;};
+        if (e == "255.255.128.0") {t.innerHTML = 17; j.innerHTML = e;};
+        if (e == "255.255.192.0") {t.innerHTML = 18; j.innerHTML = e;};
+        if (e == "255.255.224.0") {t.innerHTML = 19; j.innerHTML = e;};
+        if (e == "255.255.240.0") {t.innerHTML = 20; j.innerHTML = e;};
+        if (e == "255.255.248.0") {t.innerHTML = 21; j.innerHTML = e;};
+        if (e == "255.255.252.0") {t.innerHTML = 22; j.innerHTML = e;};
+        if (e == "255.255.254.0") {t.innerHTML = 23; j.innerHTML = e;};
+        if (e == "255.255.255.0") {t.innerHTML = 24; j.innerHTML = e;};
+        if (e == "255.255.255.128") {t.innerHTML = 25; j.innerHTML = e;};
+        if (e == "255.255.255.192") {t.innerHTML = 26; j.innerHTML = e;};
+        if (e == "255.255.255.224") {t.innerHTML = 27; j.innerHTML = e;};
+        if (e == "255.255.255.240") {t.innerHTML = 28; j.innerHTML = e;};
+        if (e == "255.255.255.248") {t.innerHTML = 29; j.innerHTML = e;};
+        if (e == "255.255.255.252") {t.innerHTML = 30; j.innerHTML = e;};
+        if (e == "255.255.255.254") {t.innerHTML = 31; j.innerHTML = e;};
+        if (e == "255.255.255.255") {t.innerHTML = 32; j.innerHTML = e;};
+    }());
+    (function() {
+        var p = document.getElementById("sm").innerHTML,
+            m = document.getElementById("ah"),
+            h = " " + "hosts";
+        (p >= 2) ? m.innerHTML = (Math.pow(2, (32 - p)) - 2) + h : m.innerHTML = 0 + h;
+    }());
+    (function() {
+        var p = document.cidrform.ip.value.split("."),
+            p = parseInt(p[0]),
+            g = document.getElementById("ab");
+        if ((p >= 0) && (p < 128)) g.innerHTML = "Class A";
+        if ((p >= 128) && (p < 192)) g.innerHTML = "Class B";
+        if ((p >= 192) && (p < 224)) g.innerHTML = "Class C";
+        if ((p >= 224) && (p < 240)) g.innerHTML = "Class D";
+        if ((p >= 240) && (p < 256)) g.innerHTML = "Class E";
+    }());
+    (function() {
+        var w = document.cidrform.ip.value,
+            s = document.getElementById("cd"),
+            g = document.getElementById("ef"),
+            k = document.getElementById("gh"),
+            p = document.getElementById("sm").innerHTML,
+            u = document.getElementById("ij").innerHTML;
+
+        var ipa = w.split("."),
+            sma = u.split("."),
+            zero = ipa[0],
+            one = ipa[1],
+            two = ipa[2],
+            three = ipa[3],
+            smzero = sma[0],
+            smone = sma[1],
+            smtwo = sma[2],
+            smthree = sma[3];
+
+        for (var i = 0; i < sma.length; i++) {
+            if (sma[i] != "255") {
+                var remember = sma[i];
+                break;
+            }
+        };
+
+        if (remember == sma[0]) {
+            if (p % 8 == 0) {
+                var network = 128,
+                    stop = (Math.floor(ipa[0] / network)),
+                    seattle = stop * network,
+                    broadcast = network - 1,
+                    tacoma = seattle + broadcast;
+                s.innerHTML = seattle + "0.0.0";
+                g.innerHTML = tacoma + "255.255.255";
+                k.innerHTML = (Math.floor(Math.pow(2, p))) + " " + "subnets";
+            }; 
+            if (p % 8 !== 0) {
+            var original = p % 8,
+                middle = 8 - original,
+                network = (Math.pow(2, middle)),
+                broadcast = network - 1,
+                stop = (Math.floor(ipa[0] / network)),
+                seattle = stop * network,
+                tacoma = seattle + broadcast;
+            s.innerHTML = seattle + "." + ipa[1] + "." + ipa[2] + "." + ipa[3];
+            g.innerHTML = tacoma + "." + ipa[1] + "." + ipa[2] + "." + ipa[3];
+            k.innerHTML = (Math.floor(Math.pow(2, p))) + " " + "subnets";
+            };
+            var remember = null;
+        };
+        if (remember == sma[1]) {
+            if (p % 8 == 0) {
+                var network = 128,
+                    stop = (Math.floor(ipa[1] / network)),
+                    seattle = stop * network,
+                    broadcast = network - 1,
+                    tacoma = seattle + broadcast;
+                s.innerHTML = ipa[0] + "." + seattle + ".0.0";
+                g.innerHTML = ipa[0] + "." + tacoma + ".255.255";
+                k.innerHTML = (Math.floor(Math.pow(2, (p - 8)))) + " " + "subnets";
+            }; 
+            if (p % 8 !== 0) {
+            var original = p % 8,
+                middle = 8 - original,
+                network = (Math.pow(2, middle)),
+                broadcast = network - 1,
+                stop = (Math.floor(ipa[1] / network)),
+                seattle = stop * network,
+                tacoma = seattle + broadcast;
+            s.innerHTML = ipa[0] + "." + seattle + "." + ipa[2] + "." + ipa[3];
+            g.innerHTML = ipa[0] + "." + tacoma + "." + ipa[2] + "." + ipa[3];
+            k.innerHTML = (Math.floor(Math.pow(2, (p - 8)))) + " " + "subnets";
+            };
+            var remember = null;
+        };
+        if (remember == sma[2]) {
+            if (p % 8 == 0) {
+                var network = 128,
+                    stop = (Math.floor(ipa[2] / network)),
+                    seattle = stop * network,
+                    broadcast = network - 1,
+                    tacoma = seattle + broadcast;
+                s.innerHTML = ipa[0] + "." + ipa[1] + seattle + ".0";
+                g.innerHTML = ipa[0] + "." + ipa[1] + tacoma + ".255";
+                k.innerHTML = (Math.floor(Math.pow(2, (p - 16)))) + " " + "subnets";
+            }; 
+            if (p % 8 !== 0) {
+            var original = p % 8,
+                middle = 8 - original,
+                network = (Math.pow(2, middle)),
+                broadcast = network - 1,
+                stop = (Math.floor(ipa[2] / network)),
+                seattle = stop * network,
+                tacoma = seattle + broadcast;
+            s.innerHTML = ipa[0] + "." + ipa[1] + "." + seattle + "." + ipa[3];
+            g.innerHTML = ipa[0] + "." + ipa[1] + "." + tacoma + "." + ipa[3];
+            k.innerHTML = (Math.floor(Math.pow(2, (p - 16)))) + " " + "subnets";
+            };
+            var remember = null;
+        };
+        if (remember == sma[3]) {
+            if (p % 8 == 0) {
+                var network = 128,
+                    stop = (Math.floor(ipa[3] / network)),
+                    seattle = stop * network,
+                    broadcast = network - 1,
+                    tacoma = seattle + broadcast;
+                s.innerHTML = ipa[0] + "." + ipa[1] + ipa[2] + ".0";
+                g.innerHTML = ipa[0] + "." + ipa[1] + ipa[2] + ".255";
+                k.innerHTML = (Math.floor(Math.pow(2, (p - 24)))) + " " + "subnets";
+            }; 
+            if (p % 8 !== 0) {
+            var original = p % 8,
+                middle = 8 - original,
+                network = (Math.pow(2, middle)),
+                broadcast = network - 1,
+                stop = (Math.floor(ipa[3] / network)),
+                seattle = stop * network,
+                tacoma = seattle + broadcast;
+            s.innerHTML = ipa[0] + "." + ipa[1] + "." + ipa[2] + "." + seattle;
+            g.innerHTML = ipa[0] + "." + ipa[1] + "." + ipa[2] + "." + tacoma;
+            k.innerHTML = (Math.floor(Math.pow(2, (p - 24)))) + " " + "subnets";
+            };
+            var remember = null;
+        };
+
+
+
+
+
+
+    }());
+
+
+
+    (function () {
+        var lk = document.getElementById("gh");
+        if (lk.innerHTML == "0 subnets") {
+            lk.innerHTML = "1 subnets";
+        };
+    }());
+    (function() {
+        var k = document.getElementById("sm");
+        document.getElementById("sm").innerHTML = "/" + k.innerHTML;
+
+    }());
+
+
+}
