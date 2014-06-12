9d98109692bef164443663eb7847cf73c9134ce7
Eric <ericscottlagergren@gmail.com>
2014-06-12 04:41:45 -0700

    scrolling

--- src/js/master.js
+++ src/js/master.js
@@ -14,29 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-
-
-function setMyCookie() {
-   var now = new Date();
-   var expires = now.setTime(now.getTime() + 12 * 2628000 );
-   document.cookie = 'subnetting=noshow;path=/;expires='+expires.toGMTString()+';';
-}
-
-function readCookie() {
-    var nameEQ = "subnetting" + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-
 var iOS = (navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
-var readcookie = readCookie();
 
-if (window.navigator.standalone == false && iOS == true && readcookie != null) {
+if (window.navigator.standalone == false && iOS == true) {
     document.getElementById("iphoneinstall").setAttribute("class","fish");
 } else {
     document.getElementById("iphoneinstall").setAttribute("class","hidden");
