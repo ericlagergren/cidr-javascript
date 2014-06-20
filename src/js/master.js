3a2937cdd7d70062d6e43399227e7c3ab1becc23
Eric <ericscottlagergren@gmail.com>
2014-06-20 16:23:22 -0700

    js update

--- src/js/master.js
+++ src/js/master.js
@@ -1,3 +1,4 @@
+/*jshint bitwise: false*/
 /*
 Copyright 2014 Eric Lagergren
 
@@ -19,21 +20,22 @@ if ("standalone" in window.navigator && window.navigator.standalone) {
     var noddy, remotes = false;
     document.addEventListener("click", function(event) {
         noddy = event.target;
-        while (noddy.nodeName !== "A" && noddy.nodeName !== "HTML") noddy = noddy.parentNode;
+        while (noddy.nodeName !== "A" && noddy.nodeName !== "HTML") { noddy = noddy.parentNode;
+        }
         if ("href" in noddy && noddy.href.indexOf("http") !== -1 && (noddy.href.indexOf(document.location.host) !== -1 || remotes)) {
             event.preventDefault();
-            document.location.href = noddy.href
+            document.location.href = noddy.href;
         }
-    }, false)
-};
+    }, false);
+}
 
 function findios() {
     var iOS = /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
     if (iOS === true && !window.navigator.standalone) {
-        document.getElementById("iphoneinstall").setAttribute("class","fish");
-        document.getElementsByTagName("body")[0].setAttribute("style","margin-top:40px;");
+        document.getElementById("iphoneinstall").setAttribute("class", "fish");
+        document.getElementsByTagName("body")[0].setAttribute("style", "margin-top:40px;");
     } else {
-        document.getElementById("iphoneinstall").setAttribute("class","hidden");
+        document.getElementById("iphoneinstall").setAttribute("class", "hidden");
     }
 }
 findios();
@@ -41,9 +43,9 @@ findios();
 function openInstall() {
     element = document.getElementById("iphoneinstall");
     if (element.classList.contains("fish")) {
-        element.setAttribute("class","open");
+        element.setAttribute("class", "open");
     } else {
-        element.setAttribute("class","fish"); 
+        element.setAttribute("class", "fish");
     }
 }
 
@@ -53,136 +55,137 @@ function updateSite(event) {
 window.applicationCache.addEventListener('updateready', updateSite, false);
 
 function val() {
-	var doc = document,
+    var doc = document,
         submaskInput = doc.form.submask.value,
-	    ipInput = doc.form.ip.value,
-	    submask, base, ssl, index, theBigString, netInit, netFinal;
+        ipInput = doc.form.ip.value,
+        submask, base, ssl, index, theBigString, netFinal, netInit;
 
 
     // Determine the type of input
     if (submaskInput <= 32) { // less than or equal to = cidr
         base = submaskInput;
-        submask = getSubmask(parseInt(submaskInput,10));
+        submask = getSubmask(parseInt(submaskInput, 10));
     }
-    if (submaskInput.split(".").length === 4) { 
-    // if you can split the input ip into four parts it's a submask
+    if (submaskInput.split(".").length === 4) {
+        // if you can split the input ip into four parts it's a submask
         base = getCidr(submaskInput);
-    	submask = submaskInput;
+        submask = submaskInput;
     }
     if (doc.form.cb.checked || submaskInput > 32) { // greater than = host
         base = getCidrFromHost(submaskInput);
         submask = getSubmask(base);
     }
     if (base === 'undefined' || isNaN(base) || base === null) {
-    	return null
+        console.log("no submask input")
+        return null;
     }
 
-	var ipInputArray = ipInput.split("."),
-	   submaskInputArray = submask.split(".");
+    var ipInputArray = ipInput.split("."),
+        submaskInputArray = submask.split(".");
 
     function getCidrFromHost(input) {
         if (input !== 0) {
-            var input = (32 - (Math.ceil((Math.log(input))/(Math.log(2)))));
+            input = (32 - (Math.ceil((Math.log(input)) / (Math.log(2)))));
         }
         return input;
     }
 
     function getSubmask(input) {
-		if (input === 0) return "0.0.0.0";
-		if (input === 1) return "128.0.0.0";
-		if (input === 2) return "192.0.0.0";
-		if (input === 3) return "224.0.0.0";
-		if (input === 4) return "240.0.0.0";
-		if (input === 5) return "248.0.0.0";
-		if (input === 6) return "252.0.0.0";
-		if (input === 7) return "254.0.0.0";
-		if (input === 8) return "255.0.0.0";
-		if (input === 9) return "255.128.0.0";
-		if (input === 10) return "255.192.0.0";
-		if (input === 11) return "255.224.0.0";
-		if (input === 12) return "255.240.0.0";
-		if (input === 13) return "255.248.0.0";
-		if (input === 14) return "255.252.0.0";
-		if (input === 15) return "255.254.0.0";
-		if (input === 16) return "255.255.0.0";
-		if (input === 17) return "255.255.128.0";
-		if (input === 18) return "255.255.192.0";
-		if (input === 19) return "255.255.224.0";
-		if (input === 20) return "255.255.240.0";
-		if (input === 21) return "255.255.248.0";
-		if (input === 22) return "255.255.252.0";
-		if (input === 23) return "255.255.254.0";
-		if (input === 24) return "255.255.255.0";
-		if (input === 25) return "255.255.255.128";
-		if (input === 26) return "255.255.255.192";
-		if (input === 27) return "255.255.255.224";
-		if (input === 28) return "255.255.255.240";
-		if (input === 29) return "255.255.255.248";
-		if (input === 30) return "255.255.255.252";
-		if (input === 31) return "255.255.255.254";
-		if (input === 32) return "255.255.255.255";
+        if (input === 0) {return "0.0.0.0";}
+        if (input === 1) {return "128.0.0.0";}
+        if (input === 2) {return "192.0.0.0";}
+        if (input === 3) {return "224.0.0.0";}
+        if (input === 4) {return "240.0.0.0";}
+        if (input === 5) {return "248.0.0.0";}
+        if (input === 6) {return "252.0.0.0";}
+        if (input === 7) {return "254.0.0.0";}
+        if (input === 8) {return "255.0.0.0";}
+        if (input === 9) {return "255.128.0.0";}
+        if (input === 10) {return "255.192.0.0";}
+        if (input === 11) {return "255.224.0.0";}
+        if (input === 12) {return "255.240.0.0";}
+        if (input === 13) {return "255.248.0.0";}
+        if (input === 14) {return "255.252.0.0";}
+        if (input === 15) {return "255.254.0.0";}
+        if (input === 16) {return "255.255.0.0";}
+        if (input === 17) {return "255.255.128.0";}
+        if (input === 18) {return "255.255.192.0";}
+        if (input === 19) {return "255.255.224.0";}
+        if (input === 20) {return "255.255.240.0";}
+        if (input === 21) {return "255.255.248.0";}
+        if (input === 22) {return "255.255.252.0";}
+        if (input === 23) {return "255.255.254.0";}
+        if (input === 24) {return "255.255.255.0";}
+        if (input === 25) {return "255.255.255.128";}
+        if (input === 26) {return "255.255.255.192";}
+        if (input === 27) {return "255.255.255.224";}
+        if (input === 28) {return "255.255.255.240";}
+        if (input === 29) {return "255.255.255.248";}
+        if (input === 30) {return "255.255.255.252";}
+        if (input === 31) {return "255.255.255.254";}
+        if (input === 32) {return "255.255.255.255";}
     }
 
     function getCidr(input) {
-    	if (input === "0.0.0.0") return 0;
-    	if (input === "128.0.0.0") return 1;
-    	if (input === "192.0.0.0") return 2;
-    	if (input === "224.0.0.0") return 3;
-		if (input === "240.0.0.0") return 4;
-		if (input === "248.0.0.0") return 5;
-		if (input === "252.0.0.0") return 6;
-		if (input === "254.0.0.0") return 7;
-		if (input === "255.0.0.0") return 8;
-		if (input === "255.128.0.0") return 9;
-		if (input === "255.192.0.0") return 10;
-		if (input === "255.224.0.0") return 11;
-		if (input === "255.240.0.0") return 12;
-		if (input === "255.248.0.0") return 13;
-		if (input === "255.252.0.0") return 14;
-		if (input === "255.254.0.0") return 15;
-		if (input === "255.255.0.0") return 16;
-		if (input === "255.255.128.0") return 17;
-		if (input === "255.255.192.0") return 18;
-		if (input === "255.255.224.0") return 19;
-		if (input === "255.255.240.0") return 20;
-		if (input === "255.255.248.0") return 21;
-		if (input === "255.255.252.0") return 22;
-		if (input === "255.255.254.0") return 23;
-		if (input === "255.255.255.0") return 24;
-		if (input === "255.255.255.128") return 25;
-		if (input === "255.255.255.192") return 26;
-		if (input === "255.255.255.224") return 27;
-		if (input === "255.255.255.240") return 28;
-		if (input === "255.255.255.248") return 29;
-		if (input === "255.255.255.252") return 30;
-		if (input === "255.255.255.254") return 31;
-		if (input === "255.255.255.255") return 32;
+        if (input === "0.0.0.0") {return 0;}
+        if (input === "128.0.0.0") {return 1;}
+        if (input === "192.0.0.0") {return 2;}
+        if (input === "224.0.0.0") {return 3;}
+        if (input === "240.0.0.0") {return 4;}
+        if (input === "248.0.0.0") {return 5;}
+        if (input === "252.0.0.0") {return 6;}
+        if (input === "254.0.0.0") {return 7;}
+        if (input === "255.0.0.0") {return 8;}
+        if (input === "255.128.0.0") {return 9;}
+        if (input === "255.192.0.0") {return 10;}
+        if (input === "255.224.0.0") {return 11;}
+        if (input === "255.240.0.0") {return 12;}
+        if (input === "255.248.0.0") {return 13;}
+        if (input === "255.252.0.0") {return 14;}
+        if (input === "255.254.0.0") {return 15;}
+        if (input === "255.255.0.0") {return 16;}
+        if (input === "255.255.128.0") {return 17;}
+        if (input === "255.255.192.0") {return 18;}
+        if (input === "255.255.224.0") {return 19;}
+        if (input === "255.255.240.0") {return 20;}
+        if (input === "255.255.248.0") {return 21;}
+        if (input === "255.255.252.0") {return 22;}
+        if (input === "255.255.254.0") {return 23;}
+        if (input === "255.255.255.0") {return 24;}
+        if (input === "255.255.255.128") {return 25;}
+        if (input === "255.255.255.192") {return 26;}
+        if (input === "255.255.255.224") {return 27;}
+        if (input === "255.255.255.240") {return 28;}
+        if (input === "255.255.255.248") {return 29;}
+        if (input === "255.255.255.252") {return 30;}
+        if (input === "255.255.255.254") {return 31;}
+        if (input === "255.255.255.255") {return 32;}
     }
 
     function calculateHosts(hv) {
         hv = hv || 0; // zero out hv
         if (hv >= 2) {
-            hv = (Math.pow(2, (32 - hv))) 
+            hv = (Math.pow(2, (32 - hv)));
             // 2^(total bits - on bits) = off bits
         }
-        return hv
+        return hv;
     }
 
     function calculateSubnets(input) {
-    	// this is black magic >:)
+        // this is black magic >:)
         var valToSubtractFromInput = !index ? 0 : index < 3 ? Math.pow(2, index + 2) : 24;
-        return~~ Math.pow(2, (input - valToSubtractFromInput)) + " subnets"
+        return~~ Math.pow(2, (input - valToSubtractFromInput)) + " subnets";
     }
 
 
     function onBits(bits) {
         var one = "1",
-          two = "0";
+            two = "0";
         for (var i = ""; i.length < bits;) {
-            i += one
+            i += one;
         }
         for (var v = ""; v.length < (32 - bits);) {
-          v += two
+            v += two;
         }
         var binarystring = i + v;
         return binarystring.replace(/\B(?=(\d{8})+(?!\d))/g, ".");
@@ -191,35 +194,35 @@ function val() {
     function findClass(ip) {
         if (ipInputArray.length === 4) {
             if (!ip || ip < 0 || typeof ip === 'undefined') {
-                return "No Valid IP Entered"
+                return "No Valid IP Entered";
             }
             if (ip < 128) {
-                return "Class A"
+                return "Class A";
             }
             if (ip < 192) {
-                return "Class B"
+                return "Class B";
             }
             if (ip < 224) {
-                return "Class C"
+                return "Class C";
             }
             if (ip < 240) {
-                return "Class D"
+                return "Class D";
             }
             if (ip < 256) {
-                return "Class E"
+                return "Class E";
             }
         } else {
-            return "No Valid IP Entered"
+            return "No Valid IP Entered";
         }
     }
 
-    var ssl = submaskInputArray.length;
+    ssl = submaskInputArray.length;
 
-    for (var i = 0; i < ssl; i++) {
-    	// finds the first octet not equal to 255
-        if (submaskInputArray[i] !== "255") {
-            index = i;
-            break
+    for (var j = 0; j < ssl; j++) {
+        // finds the first octet not equal to 255
+        if (submaskInputArray[j] !== "255") {
+            index = j;
+            break;
         }
     }
 
@@ -228,22 +231,22 @@ function val() {
         if (modResult) {
             init = (Math.pow(2, (8 - modResult)));
             network = ((Math.floor(ipInputArray[index] / init)) * init);
-            broadcast = (network + (init - 1))
+            broadcast = (network + (init - 1));
         } else if (cider === 32 || cider === 31) {
             network = "N/A";
             broadcast = "N/A";
         } else {
             init = 128;
             network = ((Math.floor(ipInputArray[index] / init)) * init);
-            broadcast = "255"
+            broadcast = "255";
         }
-        return [network, broadcast]
+        return [network, broadcast];
     }
-    
+
     function getEnds(input) {
-    	var netInit = getNetworkRange(base),
-    		netFinal = placeRangeCorrectly(netInit[0], netInit[1]);
-    	return netFinal;
+        var netInit = getNetworkRange(base),
+            netFinal = placeRangeCorrectly(netInit[0], netInit[1]);
+        return netFinal;
     }
 
     function placeRangeCorrectly(network, broadcast) {
@@ -251,23 +254,27 @@ function val() {
             broadcastStringInitial = "";
         for (var i = 0; i < index; i++) {
             networkStringInitial += ipInputArray[i] + ".";
-            broadcastStringInitial += ipInputArray[i] + "."
+            broadcastStringInitial += ipInputArray[i] + ".";
         }
         networkString = networkStringInitial + network;
         broadcastString = broadcastStringInitial + broadcast;
         if (index === 0) {
             networkString += ".0.0.0";
-            broadcastString += ".255.255.255"
+            broadcastString += ".255.255.255";
         }
         if (index === 1) {
             networkString += ".0.0";
-            broadcastString += ".255.255"
+            broadcastString += ".255.255";
         }
         if (index === 2) {
             networkString += ".0";
-            broadcastString += ".255"
+            broadcastString += ".255";
         }
-        return theBigString = [networkString, broadcastString]
+        if (!index) {
+            networkString = ipInput;
+            broadcastString = ipInput;
+        }
+        return theBigString = [networkString, broadcastString];
     }
 
     function datRangeYo() {
@@ -276,21 +283,24 @@ function val() {
             firstUsable = (parseInt(networkOctet[3], 10) + 1),
             lastUsable = (parseInt(broadcastOctet[3], 10) - 1),
             fullUsableRange = networkOctet.slice(0, -1).join(".") + "." + firstUsable + " - " + broadcastOctet.slice(0, -1).join(".") + "." + lastUsable;
-        return fullUsableRange
+            if(!index) {
+                fullUsableRange = ipInput + " - " + ipInput;
+            }
+        return fullUsableRange;
     }
 
 
-    var ipiptoint = ipInputArray.map(function(x) { 
-        return parseInt(x, 10); 
+    var ipiptoint = ipInputArray.map(function(x) {
+        return parseInt(x, 10);
     });
     var iptohex = ipiptoint.map(function(v) {
         return ("00" + v.toString(16)).substr(-2);
     }).join(".");
     var wildcard = submaskInputArray.map(function(v) {
-        return 255 - v
+        return 255 - v;
     }).join(".");
     var hosts = calculateHosts(base),
-    	usable_hosts = (hosts - 2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        usable_hosts = (hosts - 2) > 0 ? (hosts - 2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : 0;
 
     // CIDR
     doc.getElementById("tablecidr").innerHTML = base;
@@ -302,12 +312,12 @@ function val() {
     doc.getElementById("tablenumhosts").innerHTML = hosts.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " (" + usable_hosts + " usable)";
     // # of subnets
     doc.getElementById("tablenumsubnets").innerHTML = calculateSubnets(base);
-	// Wildcard mask
-	doc.getElementById("tablewildcardmask").innerHTML = wildcard;
+    // Wildcard mask
+    doc.getElementById("tablewildcardmask").innerHTML = wildcard;
     // IP class
     doc.getElementById("tableipclass").innerHTML = findClass(ipInputArray[0]);
     // IP -> hex
-    doc.getElementById("tableiptohex").innerHTML = iptohex.toUpperCase(); 
+    doc.getElementById("tableiptohex").innerHTML = iptohex.toUpperCase();
     // Network ID
     doc.getElementById("tablenetworkid").innerHTML = getEnds()[0];
     // Broadcast Address
@@ -315,47 +325,49 @@ function val() {
     // Network ranges
     doc.getElementById("tablenetworkrange").innerHTML = datRangeYo();
 
-	function throwError() {
-		var error = "No Valid IP Entered",
+    function throwError() {
+        var error = "No Valid IP Entered",
             doc = document;
-		// IP class
-	    doc.getElementById("tableipclass").innerHTML = error;
-	    // IP -> hex
-	    doc.getElementById("tableiptohex").innerHTML = error;
-	    // Network ID
-	    doc.getElementById("tablenetworkid").innerHTML = error;
-	    // Broadcast Address
-	    doc.getElementById("tablebroadcastaddress").innerHTML = error;
-	    // Network ranges
-	    doc.getElementById("tablenetworkrange").innerHTML = error;
+        // IP class
+        doc.getElementById("tableipclass").innerHTML = error;
+        // IP -> hex
+        doc.getElementById("tableiptohex").innerHTML = error;
+        // Network ID
+        doc.getElementById("tablenetworkid").innerHTML = error;
+        // Broadcast Address
+        doc.getElementById("tablebroadcastaddress").innerHTML = error;
+        // Network ranges
+        doc.getElementById("tablenetworkrange").innerHTML = error;
 
-	}
+    }
 
-	if (ipInput.split(".").length !== 4 || ipInput === "") {
-		throwError();
-	}
+    if (ipInput.split(".").length !== 4 || ipInput === "") {
+        throwError();
+    }
     for (var i = 0; i < 4; i++) {
         var iptoint = parseInt(ipInputArray[i], 10);
         if (iptoint != ipInputArray[i] || iptoint < 0 || iptoint > 255) {
             throwError();
-        } 
-    ipInputArray[i] = iptoint;
+        }
+        ipInputArray[i] = iptoint;
     }
 
 }
 window.onload = function() {
     document.getElementsByTagName("form")[0].onsubmit = function(evt) {
         evt.preventDefault();
-        val()
+        val();
         window.scrollTo(0, document.body.scrollHeight);
     };
-    document.onkeypress = function keypressed(e){
-      var keyCode = (window.event) ? e.which : e.keyCode;
-      if (keyCode == 13) {
-        if(val())
-          document.forms['form'].submit();
-      }      
-    }
-    document.ontouchmove = function(e) {e.preventDefault()};
+    document.onkeypress = function keypressed(e) {
+        var keyCode = (window.event) ? e.which : e.keyCode;
+        if (keyCode == 13) {
+            if (val()) {
+                document.forms['form'].submit();
+            }
+        }
+    };
+    document.ontouchmove = function(e) {
+        e.preventDefault();
+    };
 };
-
